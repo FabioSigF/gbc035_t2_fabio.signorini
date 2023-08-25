a0d122eaa0ce05d95b5fee3445d81a42fbbf63b5
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2023-08-25 17:53:20 -0300

    Delete LAB01 - PROCEDURAL directory

--- LAB01 - PROCEDURAL/ex001/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("My name is Fábio and my code is 12211bcc016.");
-    }
-}
