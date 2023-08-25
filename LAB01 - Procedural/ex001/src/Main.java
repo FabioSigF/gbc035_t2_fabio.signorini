a747a6ae118900876ca224903d2c1f9a7aad8389
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2023-08-25 17:53:00 -0300

    Add files via upload

---
+++ LAB01 - Procedural/ex001/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Fábio and my code is 12211bcc016.");
+    }
+}
